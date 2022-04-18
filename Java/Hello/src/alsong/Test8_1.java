f93916c2a241b5f497c301b10c66a3308a63fa9c
JunseongHeo <oksk476@naver.com>
2022-04-18 17:29:04 +0900

    alsong

---
+++ Java/Hello/src/alsong/Test8_1.java
@@ -0,0 +1,27 @@
+// 2g, 3g, 5g 추를 이용해서 원하는 무게 만들기
+// 10개씩 있는 2g, 3g, 5g짜리 추를 조합하여 81g을 만들어보자
+// if문과 중첩 for문을 이용하자
+
+package alsong;
+
+public class Test8_1 {
+
+	public static void main(String[] args) {
+		int n = 81;
+		int fiveCount=0;
+		int threeCount=0;
+		int twoCount=0;
+		
+		for (fiveCount=0; fiveCount<10; fiveCount++) {
+			n = n-5;
+			for (threeCount=0; threeCount<10; fiveCount++) {
+				n = n-3;
+			}
+		}
+		
+		System.out.println(n);
+		
+		System.out.println("5g 추 : "+ fiveCount + "\n3g 추 : " + threeCount + "\n2g 추 : " + twoCount);
+	}
+}
+
