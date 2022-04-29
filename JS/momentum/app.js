568574c033c52c162ca58466effcf9ad79f309cc
JunseongHeo <oksk476@naver.com>
2022-04-29 14:23:40 +0900

    js

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,2 +1,11 @@
 alert("hi");
 
+const amIFat0 = true; // definde 켜져있음 
+const amIFat = false; // definde 꺼져있음
+const amIFat2 = null; // defined 비어있음
+let something; // undefined 값정의되지 않음
+
+
+console.log(amIFat2);
+console.log("null");
+console.log(something); // undefined
