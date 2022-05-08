0826522b1a7fae43f87a18f01b00030133287cc3
JunseongHeo <oksk476@naver.com>
2022-05-08 20:51:23 +0900

    Update app.js

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,2 +1,18 @@
-alert("hi");
+let a = 10; // 새로운 변수를 생성. 이후 변수값 바꿀수 있음.
+const b = 3; // constant 상수. 값이 바뀔수 없음
+const myName = "junseong"
+let myName2 = "junseong"
+var c = 10;
 
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello "+myName);
+console.log(c);
+
+a = 12;
+// myName = "Heojunseong"; // 에러. 상수값은 변경될수 없음.
+myName2= "Heojunseong"; // 변수값 변경
+
+console.log("hello "+myName2);
