0204ae4a4cd384fe62a455c0237959843244a7d6
JunseongHeo <oksk476@naver.com>
2022-05-13 17:07:00 +0900

    051

---
+++ Java/Hello/src/ch16/MemberMain.java
@@ -0,0 +1,12 @@
+package ch16;
+
+import java.util.ArrayList;
+
+public class MemberMain { // 메인클래스로 프로그램에 사용할 리스트를 생성하고
+						// 프로그램 메뉴를 시작한다.
+	public static void main(String[] args) {
+		ArrayList<Member> data = new ArrayList<Member>();
+		MemberMenu mm = new MemberMenu();
+		mm.menuProcess(data);
+	}
+}
