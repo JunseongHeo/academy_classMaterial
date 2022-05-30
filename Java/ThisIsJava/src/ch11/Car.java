ebbd87993c862cc5829ff09ead22c9da8c24b0c9
JunseongHeo <oksk476@naver.com>
2022-05-30 17:09:23 +0900

    0530

--- Java/ThisIsJava/src/ch11/Car.java
+++ Java/ThisIsJava/src/ch11/Car.java
@@ -2,8 +2,24 @@ package ch11;
 
 public class Car {
 	public String model;
+	public String owner;
 	
+	public Car() {
+	}
 	public Car(String model) {
 		this.model = model;
 	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getModel() {
+		return model;
+	}
+	private void setOwner(String Owner) {
+		this.owner = owner;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	
 }
