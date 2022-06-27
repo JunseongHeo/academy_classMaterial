c023267a14ef9c0c12311fdd14ca2b168494baf7
JunseongHeo <oksk476@naver.com>
2022-06-27 12:22:55 +0900

    0627 자바 format 클래스

---
+++ Java/ThisIsJava/src/ch11/DecimalFormatExample.java
@@ -0,0 +1,50 @@
+package ch11;
+
+import java.text.DecimalFormat;
+
+public class DecimalFormatExample {
+	public static void main(String[] args) {
+		double num = 1234567.89;
+		
+		DecimalFormat df = new DecimalFormat("0");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("0.0");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("00000000.00000");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("#");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("#.#");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("########.#####");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("#.0");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("+#.0");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("#,###.0");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("0.0E0");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("+#,### ; -#,###");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("#.#%");
+		System.out.println(df.format(num));
+		
+		df = new DecimalFormat("¿øÈ­ #,###");
+		System.out.println(df.format(num));
+		
+	}
+
+}
