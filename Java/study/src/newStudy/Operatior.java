341f1c879e93eabf43e161602dea9a297bd7646c
JunseongHeo <oksk476@naver.com>
2022-07-13 12:01:33 +0900

    자바스크립트 배열

---
+++ Java/study/src/newStudy/Operatior.java
@@ -0,0 +1,11 @@
+package newStudy;
+
+public class Operatior {
+
+	public static void main(String[] args) {
+		int a=3, b=4, c=3, d=5;
+		
+		System.out.println((a==2 | a==c));
+		System.out.println((a==2 || a==c));
+	}
+}
