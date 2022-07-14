3aa9e99923562d548ffa12ce135264966f9c3498
JunseongHeo <oksk476@naver.com>
2022-07-14 20:13:29 +0900

    래퍼클래스

---
+++ Java/study/src/newStudy/WrapperClase.java
@@ -0,0 +1,23 @@
+package newStudy;
+
+public class WrapperClase {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		Integer i1 = new Integer(10);
+		Integer i2 = new Integer(20);
+		
+		System.out.println(i1+i2);
+		
+		final Integer i3 = new Integer(10);
+		Integer i4 = new Integer(20);
+		
+		System.out.println(i3+i4);
+		
+	
+		
+		
+	}
+}
