26fa2a471abb13b6fd95c42478a30889262d1b65
JunseongHeo <oksk476@naver.com>
2022-08-02 16:35:45 +0900

    add: 0802 시험

--- Java/study/src/newStudy/Sort_KorEngMath.java
+++ Java/study/src/newStudy/Sort_KorEngMath.java
@@ -13,19 +13,6 @@ public class Sort_KorEngMath {
 		}
 		Integer[] score = new Integer[12];
 		
-		score[0]  = { 50, 60, 100 };
-		
-		String[1]  = { "80", "60", "50" };
-		String[2]  = { "80", "70", "100" };
-		String[3]  = { "50", "60", "90" };
-		String[4]  = { "50", "60", "100" };
-		String[5]  = { "60", "80", "100" };
-		String[6]  = { "80", "60", "100" };
-		String[7]  = { "70", "70", "70" };
-		String[8]  = { "70", "70", "90" };
-		String[9]  = { "70", "70", "80" };
-		String[10]  = { "80", "80", "80" };
-		String[11]  = { "50", "60", "90" };
 
 		Student[] array = {
 				
